a63b2f85ff47eb733f844469b120503db4646e29
Muhammed Qureshi <78896173+LilAliTech@users.noreply.github.com>
2022-10-16 08:57:22 +0100

    Add files via upload

---
+++ src/components/navbar/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+Nav,
+NavLink,
+Bars,
+NavMenu,
+NavBtn,
+NavBtnLink,
+} from './NavbarElements';
+
+const Navbar = () => {
+return (
+	<>
+	<Nav>
+		<Bars />
+
+		<NavMenu>
+		<NavLink to='/about' activeStyle>
+			About
+		</NavLink>
+		<NavLink to='/events' activeStyle>
+			Projects
+		</NavLink>
+		<NavLink to='/annual' activeStyle>
+			Services
+		</NavLink>
+		<NavLink to='/blogs' activeStyle>
+			Blogs
+		</NavLink>
+		<NavLink to='/sign-up' activeStyle>
+			Contact
+		</NavLink>
+		{/* Second Nav */}
+		{/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+		</NavMenu>
+	</Nav>
+	</>
+);
+};
+
+export default Navbar;
