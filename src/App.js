3e140f74af65279a0dcb26f183bf7bf83e69833a
Muhammed Qureshi <78896173+LilAliTech@users.noreply.github.com>
2022-10-27 13:12:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar/NavbarElements';
+import Navbar from './components/Navbar/index';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages';
 import About from '..src/pages/about';
